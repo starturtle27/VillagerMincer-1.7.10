f4ff7b444eb9864be0e79d9e7dce92fc39684fcd
starlight <159906735+starturtle27@users.noreply.github.com>
2025-08-29 19:42:14 -0700

    Add files via upload

---
+++ src/main/java/com/starturtle27/villagermincer/jxLib/entity/ai/IFollowingEntity.java
@@ -0,0 +1,7 @@
+package _jx.jxLib.entity.ai;
+
+import net.minecraft.entity.EntityLiving;
+
+public interface IFollowingEntity {
+   EntityLiving getOwner();
+}
---
+++ src/main/java/com/starturtle27/villagermincer/jxLib/entity/ai/IShootingEntity.java
@@ -0,0 +1,7 @@
+package _jx.jxLib.entity.ai;
+
+import net.minecraft.entity.IRangedAttackMob;
+
+public interface IShootingEntity extends IRangedAttackMob {
+   boolean isShootingMode();
+}
