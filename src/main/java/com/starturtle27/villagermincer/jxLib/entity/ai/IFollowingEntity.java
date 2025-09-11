cd02655214625e8f9b2803616334f472c4257b86
starlight <159906735+starturtle27@users.noreply.github.com>
2025-09-10 20:37:32 -0700

    Delete src/main/java/com/starturtle27/villagermincer/jxLib/entity directory

--- src/main/java/com/starturtle27/villagermincer/jxLib/entity/ai/IFollowingEntity.java
+++
@@ -1,7 +0,0 @@
-package _jx.jxLib.entity.ai;
-
-import net.minecraft.entity.EntityLiving;
-
-public interface IFollowingEntity {
-   EntityLiving getOwner();
-}
--- src/main/java/com/starturtle27/villagermincer/jxLib/entity/ai/IShootingEntity.java
+++
@@ -1,7 +0,0 @@
-package _jx.jxLib.entity.ai;
-
-import net.minecraft.entity.IRangedAttackMob;
-
-public interface IShootingEntity extends IRangedAttackMob {
-   boolean isShootingMode();
-}
